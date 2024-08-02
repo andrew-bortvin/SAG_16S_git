a111aadbf00316e1f8eb298b3548c13fd954c5f6
andrew-bortvin <abortvi2@jh.edu>
2024-08-02 02:06:33 -0400

    updated vsearch script

--- SILVA_analysis/vsearch_16S.sh
+++ SILVA_analysis/vsearch_16S.sh
@@ -12,6 +12,10 @@ echo "Copying ${fName} to current directory"
 cp ../SAG_fqGz/${fName} ./
 gunzip ${fName}
 
+echo "Converting to fastq"
+sed -n '1~4s/^@/>/p;2~4p' ${fNamePrefix}_FD_read2.fastq  > ${fNamePrefix}_FD_read2.fasta
+sed -i 's/ /_/g' ${fNamePrefix}_FD_read2.fasta
+
 vsearch --threads 30 --usearch_global ${fName} -db ../SILVA_db/SILVA_138.1_SSURef_NR99_CYANO_ONLY.fasta --id 0.9 --blast6out vsearch_out/${fNamePrefix}.b6
 
 rm ${fNamePrefix}_FD_read2.fastq
